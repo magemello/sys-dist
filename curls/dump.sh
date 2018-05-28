dd2cafc254baf369558f6d11bdbcb755a6d326e5
Mario Romano <dnamario@hotmail.com>
2018-05-28 15:30:02 +0100

    log and gateway error

--- curls/dump.sh
+++ curls/dump.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 curl http://127.0.0.1:3001/storage/dump 1>/dev/null 2>/dev/null &
-curl http://127.0.0.2:3002/storage/dump 1>/dev/null 2>/dev/null &
-curl http://127.0.0.3:3003/storage/dump 1>/dev/null 2>/dev/null &
-curl http://127.0.0.4:3004/storage/dump 1>/dev/null 2>/dev/null &
-curl http://127.0.0.5:3005/storage/dump 1>/dev/null 2>/dev/null &
+curl http://127.0.0.1:3002/storage/dump 1>/dev/null 2>/dev/null &
+curl http://127.0.0.1:3003/storage/dump 1>/dev/null 2>/dev/null &
+curl http://127.0.0.1:3004/storage/dump 1>/dev/null 2>/dev/null &
+curl http://127.0.0.1:3005/storage/dump 1>/dev/null 2>/dev/null &
