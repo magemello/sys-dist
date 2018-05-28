e6fcbacfc2c33260fb3e5b8f3fc2f972a7d8c475
Mario Romano <dnamario@hotmail.com>
2018-05-28 16:36:24 +0100

    parametrize timeout client

--- node/src/main/java/org/magemello/sys/node/clients/ACProtocolClient.java
+++ node/src/main/java/org/magemello/sys/node/clients/ACProtocolClient.java
@@ -1,9 +1,9 @@
 package org.magemello.sys.node.clients;
 
-import org.magemello.sys.node.domain.Record;
 import org.magemello.sys.node.domain.Transaction;
 import org.magemello.sys.node.service.P2PService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
@@ -17,27 +17,30 @@ import java.time.Duration;
 @Service
 public class ACProtocolClient {
 
+    @Value("${client.timeout:3}")
+    private Integer clientTimeout;
+
     @Autowired
     private P2PService p2pService;
 
     public Mono<Boolean> propose(Transaction transaction) {
         return Flux.fromIterable(p2pService.getPeers())
                 .flatMap(peer -> createWebClientPropose(transaction, peer), p2pService.getPeers().size())
-                .timeout(Duration.ofSeconds(10))
+                .timeout(Duration.ofSeconds(clientTimeout))
                 .all(response -> !response.statusCode().isError());
     }
 
     public Mono<Boolean> commit(String id) {
         return Flux.fromIterable(p2pService.getPeers())
                 .flatMap(peer -> createWebClientCommit(id, peer), p2pService.getPeers().size())
-                .timeout(Duration.ofSeconds(10))
+                .timeout(Duration.ofSeconds(clientTimeout))
                 .all(response -> !response.statusCode().isError());
     }
 
     public Mono<Boolean> rollback(String id) {
         return Flux.fromIterable(p2pService.getPeers())
                 .flatMap(peer -> createWebClientRollBack(id, peer), p2pService.getPeers().size())
-                .timeout(Duration.ofSeconds(10))
+                .timeout(Duration.ofSeconds(clientTimeout))
                 .all(response -> !response.statusCode().isError());
     }
 
--- node/src/main/java/org/magemello/sys/node/clients/APProtocolClient.java
+++ node/src/main/java/org/magemello/sys/node/clients/APProtocolClient.java
@@ -4,6 +4,8 @@ import org.magemello.sys.node.domain.Record;
 import org.magemello.sys.node.domain.Transaction;
 import org.magemello.sys.node.service.P2PService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.ClientResponse;
@@ -16,13 +18,16 @@ import java.time.Duration;
 @Service
 public class APProtocolClient {
 
+    @Value("${client.timeout:3}")
+    private Integer clientTimeout;
+
     @Autowired
     private P2PService p2pService;
 
     public Mono<Long> propose(Transaction transaction) {
         return Flux.fromIterable(p2pService.getPeers())
                 .flatMap(peer -> createWebClientPropose(transaction, peer), p2pService.getPeers().size())
-                .timeout(Duration.ofSeconds(10))
+                .timeout(Duration.ofSeconds(clientTimeout))
                 .filter(response -> !response.statusCode().isError())
                 .count();
     }
@@ -30,7 +35,7 @@ public class APProtocolClient {
     public Mono<Long> commit(String id) {
         return Flux.fromIterable(p2pService.getPeers())
                 .flatMap(peer -> createWebClientCommit(id, peer), p2pService.getPeers().size())
-                .timeout(Duration.ofSeconds(10))
+                .timeout(Duration.ofSeconds(clientTimeout))
                 .filter(response -> !response.statusCode().isError())
                 .count();
     }
@@ -38,14 +43,14 @@ public class APProtocolClient {
     public Mono<Boolean> rollback(String id) {
         return Flux.fromIterable(p2pService.getPeers())
                 .flatMap(peer -> createWebClientRollBack(id, peer), p2pService.getPeers().size())
-                .timeout(Duration.ofSeconds(10))
+                .timeout(Duration.ofSeconds(clientTimeout))
                 .all(response -> !response.statusCode().isError());
     }
 
     public Mono<Boolean> repair(Record record) {
         return Flux.fromIterable(p2pService.getPeers())
                 .flatMap(peer -> createWebClientRepair(record, peer), p2pService.getPeers().size())
-                .timeout(Duration.ofSeconds(10))
+                .timeout(Duration.ofSeconds(clientTimeout))
                 .all(response -> !response.statusCode().isError());
     }
 
@@ -57,41 +62,45 @@ public class APProtocolClient {
     private Mono<ClientResponse> createWebClientPropose(Transaction transaction, String peer) {
         return WebClient.create()
                 .post()
-                .uri("http://localhost:" + peer + "ap/propose")
+                .uri("http://" + peer + "/ap/propose")
                 .syncBody(transaction)
                 .accept(MediaType.APPLICATION_JSON)
-                .exchange();
+                .exchange()
+                .onErrorResume(throwable -> Mono.just(ClientResponse.create(HttpStatus.BAD_GATEWAY).build()));
     }
 
     private Mono<ClientResponse> createWebClientCommit(String id, String peer) {
         return WebClient.create()
                 .post()
-                .uri("http://localhost:" + peer + "ap/commit/" + id)
+                .uri("http://" + peer + "/ap/commit/" + id)
                 .accept(MediaType.APPLICATION_JSON)
-                .exchange();
+                .exchange()
+                .onErrorResume(throwable -> Mono.just(ClientResponse.create(HttpStatus.BAD_GATEWAY).build()));
     }
 
     private Mono<ClientResponse> createWebClientRollBack(String id, String peer) {
         return WebClient.create()
                 .post()
-                .uri("http://localhost:" + peer + "ap/rollback/" + id)
+                .uri("http://" + peer + "/ap/rollback/" + id)
                 .accept(MediaType.APPLICATION_JSON)
-                .exchange();
+                .exchange()
+                .onErrorResume(throwable -> Mono.just(ClientResponse.create(HttpStatus.BAD_GATEWAY).build()));
     }
 
     private Mono<ClientResponse> createWebClientRepair(Record record, String peer) {
         return WebClient.create()
                 .post()
-                .uri("http://localhost:" + peer + "ap/repair")
+                .uri("http://" + peer + "/ap/repair")
                 .syncBody(record)
                 .accept(MediaType.APPLICATION_JSON)
-                .exchange();
+                .exchange()
+                .onErrorResume(throwable -> Mono.just(ClientResponse.create(HttpStatus.BAD_GATEWAY).build()));
     }
 
     private Mono<Record> createWebClientRead(String key, String peer) {
         return WebClient.create()
                 .get()
-                .uri("http://localhost:" + peer + "ap/read/" + key)
+                .uri("http://" + peer + "/ap/read/" + key)
                 .accept(MediaType.APPLICATION_JSON)
                 .retrieve()
                 .bodyToMono(Record.class)
