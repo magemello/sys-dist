d5483a811ff76122b798e4a37b01565d72b3f010
Bruno Bossola <bbossola@gmail.com>
2018-05-28 16:17:06 +0100

    Added config in script for 127.0.0.x servers

--- scripts/node.sh
+++ scripts/node.sh
@@ -1,10 +1,13 @@
 #!/usr/bin/env bash
 
-if [ -z "$1" ] 
+if [ -z "$1" ]
   then
     echo "No port specified"
     exit 1
 fi
 
+iden=$(echo $1-3000 | bc)
+echo Going to start 127.0.0.$iden:$1
+
 cd ../node
-mvn -Dserver.port=$1 spring-boot:run
+mvn -Dserver.address=127.0.0.$iden -Dserver.port=$1 spring-boot:run
